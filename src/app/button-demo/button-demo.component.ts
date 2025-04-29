972e9968b846522c37b1420c8157a657e9fcc4a5
safwen elmastouri <safwen.elmastouri@gmail.com>
2025-04-29 20:29:22 +0100

    Fix project config

--- src/app/button-demo/button-demo.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    templateUrl: './button-demo.component.html',
-    styleUrls: ['./button-demo.component.css'],
-})
-export class ButtonDemo {}
